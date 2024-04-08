c83ee8be9b28ee5fb5c7d049675852602f8c1ef0
Prithvi2310 <shahprithvi1505@gmail.com>
2024-04-09 01:13:25 +0530

    created custom exception handling class

---
+++ src/exception.py
@@ -0,0 +1,23 @@
+import sys
+
+# custom exception function
+def error_message_details(error, error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()  # gets information about the exception from the interpreter
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    lineNo = exc_tb.tb_lineno
+    error_message = "error occured in script [{0}] on line number [{1}]; error message [{2}]".format(
+        file_name,
+        lineNo,
+        str(error)
+    )
+    
+    return error_message
+    
+# defining a custom exception class which inherits from the base exception class
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        super.__init__(error_message)  # calling the parent class constructor
+        self.error_message = error_message_details(error_message,error_detail)
+        
+    def __str__(self):
+        return self.error_message
