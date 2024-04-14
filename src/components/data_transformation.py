c0efeed0d8463514e36444e7cb12600a4cf32b4e
Prithvi2310 <shahprithvi1505@gmail.com>
2024-04-14 22:43:42 +0530

    error fixed

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,29 +1,31 @@
-import os
 import sys
-from  dataclasses import dataclass
+from dataclasses import dataclass
 
-import numpy as np
+import numpy as np 
 import pandas as pd
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
+import os
+
 from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path =  os.path.join('artifact','preprocessor.pkl')
-    
+    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+
 class DataTransformation:
     def __init__(self):
-        self.data_transformation_config = DataTransformationConfig()
-        
-    def get_data_transformer_obj(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+    def get_data_transformer_object(self):
         '''
-        This function is responsible for the data transformation pipeline
+        This function si responsible for data trnasformation
+        
         '''
         try:
             numerical_columns = ["writing_score", "reading_score"]
@@ -34,77 +36,83 @@ class DataTransformation:
                 "lunch",
                 "test_preparation_course",
             ]
-            
-            num_pipeline = Pipeline(
+
+            num_pipeline= Pipeline(
                 steps=[
-                    ("Imputer",SimpleImputer(strategy="median")),
-                    ("Scaler",StandardScaler())
+                ("imputer",SimpleImputer(strategy="median")),
+                ("scaler",StandardScaler())
+
                 ]
             )
-            
-            cat_pipeline = Pipeline(
+
+            cat_pipeline=Pipeline(
+
                 steps=[
-                    ("imputer",SimpleImputer(strategy="most_frequent")),
-                    ("one_hot_encoder",OneHotEncoder()),
-                    ("Scaler",StandardScaler())
+                ("imputer",SimpleImputer(strategy="most_frequent")),
+                ("one_hot_encoder",OneHotEncoder()),
+                ("scaler",StandardScaler(with_mean=False))
                 ]
+
             )
-            
-            logging.info("Numerical columns: ", numerical_columns)
-            logging.info("Categorical columns: ", categorical_columns)
-            
-            preprocessor = Pipeline(
-                steps=[
-                    ("num_pipeline",num_pipeline,numerical_columns),
-                    ("cat_pipeline",cat_pipeline,categorical_columns)   
+
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")
+
+            preprocessor=ColumnTransformer(
+                [
+                ("num_pipeline",num_pipeline,numerical_columns),
+                ("cat_pipelines",cat_pipeline,categorical_columns)
+
                 ]
+
+
             )
-            
+
             return preprocessor
         
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(e,sys)
         
-        
-    def initiate_data_transformation(self, train_path, test_path):
+    def initiate_data_transformation(self,train_path,test_path):
+
         try:
-            train_df = pd.read_csv(train_path)
-            test_df = pd.read_csv(test_path)
-            
-            logging.info("Read training and testing sets")
-            
-            logging.info("Obtaining the preprocessor object")
-            
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+
+            logging.info("Read train and test data completed")
+
+            logging.info("Obtaining preprocessing object")
+
             # this object enables to use the preprocessor pipeline
-            preprocessor_obj = self.get_data_transformer_obj()
-            
-            target_column_name = "math_score"
+            preprocessing_obj=self.get_data_transformer_object()
+
+            target_column_name="math_score"
             numerical_columns = ["writing_score", "reading_score"]
-            
+
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
-            
+
             input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
             target_feature_test_df=test_df[target_column_name]
 
             logging.info(
-                "Applying preprocessing object on training dataframe and testing dataframe."
+                f"Applying preprocessing object on training dataframe and testing dataframe."
             )
-            
-            logging.info(input_feature_train_df.shape)
-            
-            input_feature_train_arr=preprocessor_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr=preprocessor_obj.transform(input_feature_test_df)
-            
-            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
-            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)] 
+
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
             logging.info(f"Saved preprocessing object.")
-            
+
             save_object(
 
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
-                obj=preprocessor_obj
+                obj=preprocessing_obj
 
             )
 
